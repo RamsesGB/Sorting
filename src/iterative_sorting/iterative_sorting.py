0362a0ce8b25f6bb3424029cd9d0131bcd635e0a
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-10-08 11:59:47 -0400

    finished MVP for day 1

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,29 +1,43 @@
 # TO-DO: Complete the selection_sort() function below 
+l = [12,4,15,3,7,2,1,14,6]
+
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+    for i in range(0, len(arr)):
+        #assign the smallest number (at first this will point to the first unsorted element in the list aka index 0)
+        index_of_smallest_num = i
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        # Note that j starts 1 index ahead of the parent loop
+        for j in range(i+1, len(arr)):
+            if arr[index_of_smallest_num] > arr[j]:
+                '''Upon finding the smallest unsorted number, reassign the value of 
+                index_of_smallest_num to the new position (index) at which the smallest 
+                number in the array is found'''
+                index_of_smallest_num = j
 
-
-
-        # TO-DO: swap
-
-
-
+        temp = arr[i]
+        arr[i] = arr[index_of_smallest_num]
+        arr[index_of_smallest_num] = temp
 
     return arr
+
+print(selection_sort(l))
+
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    #loops through every element of the given array
+    for i in range(0, len(arr) -1):
+        #loops through the array and swaps the value on the left for
+        #the value on the right if the value on the right is bigger
+        for i in range(0, len(arr) -1):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
     return arr
 
 
+##################################################
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
 
